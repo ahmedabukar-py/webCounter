1c106df78305885323def76599c0fbefeaba4b1b
Ahmed <ahmed-abdirahman@outlook.com>
2025-04-18 17:27:10 +0100

    removed profile from provider and editted backend.yml

--- Backend/Terraform/provider.tf
+++ Backend/Terraform/provider.tf
@@ -16,5 +16,5 @@ provider "aws" {
 provider "aws" {
   region                   = var.region
   shared_credentials_files = ["/Users/ahmedabdirahman/.aws/credentials"]
-  profile                  = "vscode"
+  //profile                  = "vscode"
 }
